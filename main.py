074daa9aab322b5aec5118cddf81f73c270f2ef2
TheIceBladeNetwork <TheIceBladeNetwork@gmail.com>
2017-04-02 23:26:38 -0500

    changed some stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import webapp2
-
+from validate_email import validate_email
 class MainHandler(webapp2.RequestHandler):
     def get(self, username = "", password = "", confirm = "", error = "", errorsym2 = "", errorsym1 = "", errorsym0 = ""):
         header = """
@@ -18,16 +18,16 @@ class MainHandler(webapp2.RequestHandler):
                 <form method="post">
                     <table>
                         <tr>
-                            <td><p>Username<span class="error">%(errorsym0)s</span></p></td>
-                            <td><input type="text" name="username" value="%(username)s"></td>
+                            <td><p>Username</td>
+                            <td><input type="text" name="username" value="%(username)s" required><span class="error">%(errorsym0)s</span></p></td>
                         </tr>
                         <tr>
-                            <td><p>Password<span class="error">%(errorsym1)s</span></p></td>
-                            <td><input type="password" name="password" value="%(password)s"></td>
+                            <td><p>Password</td>
+                            <td><input type="password" name="password" value="%(password)s" required><span class="error">%(errorsym1)s</span></p></td>
                         </tr>
                         <tr>
-                            <td><p>Comfirm Password<span class="error">%(errorsym2)s</span></p></td>
-                            <td><input type="password" name="confirm" value="%(confirm)s"></td>
+                            <td><p>Comfirm Password</p></td>
+                            <td><input type="password" name="confirm" value="%(confirm)s" required><span class="error">%(errorsym2)s</span></td>
                         </tr>
                         <tr>
                             <td><p>Email (Optional)</p></td>
@@ -47,15 +47,9 @@ class MainHandler(webapp2.RequestHandler):
         username = self.request.get("username")
         password = self.request.get("password")
         confirm = self.request.get("confirm")
-        error = "<p class='error'>Inputs with a '*' are invalid!</p>"
-        if username == "" and password == "":
-            self.get(username, password, confirm, error, "*", "*", "*")
-        elif username == "":
-            self.get(username, password, confirm, error, "", "", "*")
-        elif password == "":
-            self.get(username, password, confirm, error, "*", "*")
-        elif password != confirm:
-            self.get(username, password, confirm, error, "*")
+        email = self.request.get("email")
+        if password != confirm or " " in username or (len(email) > 0 and validate_email(email, verify=True)):
+            self.get(username, password, confirm, error, email)
         else:
             self.redirect("/welcome?user=" + username)
                     
